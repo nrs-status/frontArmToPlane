0bdfb3fd651123310fdf2f4ce70bcb1ac94d99e6
nrs-status <sebaarsim@gmail.com>
2025-04-23 13:04:22 -0300

    changing default to nixvim

---
+++ temple_artemis_ephesus/montezuma_circles_scroll/nixvim.nix
@@ -0,0 +1,12 @@
+{ inputs, system }:
+let
+  mkNixvim = inputs.libs.baselib.preMkNixvim {
+    inherit inputs;
+    inherit system;
+  };
+in
+{
+  base = mkNixvim (import ./envAttrs/base.nix {});
+  lean = mkNixvim (import ./envAttrs/forLean.nix { inherit inputs; });
+  webDev = mkNixvim (import ./envAttrs/forWebDev.nix { inherit inputs; });
+}
