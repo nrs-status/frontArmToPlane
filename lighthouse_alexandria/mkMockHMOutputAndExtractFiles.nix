edb7b5d98bf9143ee8aac7b01aaf3f436c5e77ab
nrs-status <sebaarsim@gmail.com>
2025-04-29 17:40:40 -0300

    discovered way to simplify mkMockHMOutput

--- lighthouse_alexandria/mkMockHMOutputAndExtractFiles.nix
+++ lighthouse_alexandria/mkMockHMOutputAndExtractFiles.nix
@@ -1,4 +1,4 @@
-{ elemAtIndex, stateVersion, homeManagerFlake, attrsOfPathsStartingAtPlaceholderHome, mockHomeExtensions, activateDebug ? false, ... }:
+{ stateVersion, homeManagerFlake, attrsOfPathsStartingAtPlaceholderHome, mockHomeExtensions, activateDebug ? false, ... }:
 #I don't understand how what's at the index given by elemAtArg is determined
 with builtins;
 let total = rec {
@@ -8,7 +8,7 @@ let total = rec {
   transformPathsIntoStringsToFetchFiles = mapAttrs (key: val: ".config" + (toString val)) attrsOfPathsStartingAtPlaceholderHome;
   grabMatchingTarget = targetToMatch: acc: next: if next.target == targetToMatch then next else acc;
   toFetchers = mapAttrs (_key: val: grabMatchingTarget val) transformPathsIntoStringsToFetchFiles;
-  mockHomeConfigFilesInternalAttrs = attrValues (elemAt homeManagerOutput.options.home.file.definitionsWithLocations elemAtIndex).value; #need to use attrValues because the keys are paths and nix won't accpet a string to acces them
+  mockHomeConfigFilesInternalAttrs = attrValues (homeManagerOutput.config.home.file); #need to use attrValues because the keys are paths and nix won't accpet a string to acces them
   handler = result: if result == {} then throw ("createMockHMOutputAndExtractFiles.nix: unallowed empty set while fetching files") else result;
   withFetched = mapAttrs (_key: val: handler (foldl' val {} mockHomeConfigFilesInternalAttrs)) toFetchers;
   withFileContents = mapAttrs (_key: val: readFile val.source) withFetched;
