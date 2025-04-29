be578dc805c1d8939816e1395ee340049c248d58
nrs-status <sebaarsim@gmail.com>
2025-04-29 00:46:01 -0300

    adding nixpkgs to byproducts for nixpkgs.nixosSystem

--- lighthouse_alexandria/mkOutput.nix
+++ lighthouse_alexandria/mkOutput.nix
@@ -12,8 +12,8 @@ let total = rec {
   mkSelectedPackages = reader: import ./mkSelectedPackages.nix {
     inherit lclpkgsdir reader;
   };
-  initReaderWith = funcToApply: declKey: declVal: genAttrsBootstrap declVal.supportedSystems (system: { ${declKey} = funcToApply (reader (rec {
-    inherit declKey;
+  initReaderWith = funcToApply: _declKey: declVal: genAttrsBootstrap declVal.supportedSystems (system: { ${declKey} = funcToApply (reader (rec {
+    nixpkgs = declVal.nixpkgs;
     pkgs = import declVal.nixpkgs {
       inherit system;
       config = declVal.nixpkgsConfig;
@@ -36,7 +36,7 @@ let total = rec {
   foldIntoDevShellsVal = builtins.foldl' deepMerge {} (attrValues selectedEnvs);
 
   mkByproduct = reader: with reader; {
-    inherit pkgs lclPkgs lclInputs types;
+    inherit nixpkgs pkgs lclPkgs lclInputs types;
   };
   byproducts = mapAttrs (initReaderWith mkByproduct) outputDeclAttrs;
   clipFirstAttr = builtins.foldl' deepMerge {} (attrValues byproducts);
