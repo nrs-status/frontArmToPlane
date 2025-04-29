4a02f27afe4bcd31b864f02a37bf1733549fc602
nrs-status <sebaarsim@gmail.com>
2025-04-28 22:52:54 -0300

    working workEnv following factoring out system from outputDecl nixpkgs call

--- flake.nix
+++ flake.nix
@@ -1,43 +1,56 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+    nixpkgsUnstable.url = "github:NixOs/nixpkgs/nixos-unstable";
     nixvimFlake.url = "github:nix-community/nixvim";
     nixpkgs2411.url = "github:NixOs/nixpkgs/nixos-24.11";
   };
 
   outputs = inputs: 
       let total = rec {
-        outputDecl1 = {
-          nixpkgs = inputs.nixpkgs;
-          nixpkgsConfig = {
-            allowUnfree = true;
-            android_sdk.accept_license = true;
-          };
-          types = lclInputs: lclInputs.baselib.mkTypesAttrs { typesdir = ./mauso_halicarnassus; importsToPass = { inherit lclInputs; }; };
-          lclInputs = pkgs: rec {
+        lclInputslessTypes = lclInputs: lclInputs.baselib.mkTypesAttrs { 
+          typesdir = ./mauso_halicarnassus; 
+          importsToPass = { inherit lclInputs; }; 
+        };
+        nixpkgslessLclInputs = pkgs: rec {
             pkgslib = pkgs.lib;
             baselib = import ./lighthouse_alexandria { inherit pkgslib; nixvimFlake = inputs.nixvimFlake; };
             tclib = import ./colossus_rhodes { inherit pkgslib baselib; };
             tc = tclib.tc;
             nixvimFlake = inputs.nixvimFlake;
-          };
-          supportedSystems = [
-            "x86_64-linux"
-          ];
-          envsToProvide = [
-            "workEnv"
-            "androidEnv"
-          ];
-          packagesToProvide = [
-            [ "nixvim" "base" ]
-            "androidSdk"
-          ];
+        };
+        supportedSystems = [
+          "x86_64-linux"
+        ];
+        nixpkgsConfig = {
+          allowUnfree = true;
+          android_sdk.accept_license = true;
+        };
+        envsToProvide = [
+          "workEnv"
+          "androidEnv"
+        ];
+        packagesToProvide = [
+          [ "nixvim" "base" ]
+          "androidSdk"
+        ];
+        outputDeclUnstable = {
+          nixpkgs = inputs.nixpkgsUnstable;
+          inherit nixpkgsConfig supportedSystems envsToProvide packagesToProvide;
+          lclInputs = nixpkgslessLclInputs;
+          types = lclInputslessTypes;
+        };
+        outputDecl2411 = {
+          nixpkgs = inputs.nixpkgs2411;
+          inherit nixpkgsConfig supportedSystems envsToProvide packagesToProvide;
+          lclInputs = nixpkgslessLclInputs;
+          types = lclInputslessTypes;
         };
         mkOutputResult = (import ./lighthouse_alexandria/mkOutput.nix) {
           envsdir = ./pyramid_giza;
           lclpkgsdir = ./temple_artemis_ephesus;
           outputDeclList = [
-            outputDecl1
+            outputDeclUnstable
+            outputDecl2411
           ];
         };
       };
