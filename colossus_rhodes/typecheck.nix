d638f1e4fd8d111d3a35250d20a0962799bc1788
nrs-status <sebaarsim@gmail.com>
2025-04-30 15:09:24 -0300

    fixing typechecking

--- colossus_rhodes/typecheck.nix
+++ colossus_rhodes/typecheck.nix
@@ -6,7 +6,7 @@ let total = rec {
     inherit target type;
   };
   predMap = map tcPred type.preds;
-  evalPreds = seq predMap target;
+  evalPreds = seq (seq predMap predMap) target;
   final = evalPreds;
   forDebug = { inherit type; };
 }; in baselib.wrapSubstitutingDebug {
