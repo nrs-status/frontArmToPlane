feccd48d723206342c4df374c807fd5b929cd460
nrs-status <sebaarsim@gmail.com>
2025-04-30 15:47:06 -0300

    splitting lighthouse_alexandria

---
+++ bill_projects_belt/default.nix
@@ -0,0 +1,9 @@
+{ pkgslib }:
+{
+  importPairAttrsOfDir = import ./importPairAttrsOfDir.nix { inherit pkgslib; };
+  listDirsSatisfyingPred = import ./listDirsSatisfyingPred.nix { inherit pkgslib };
+  mkImportPair = import ./mkImportPair.nix { inherit pkgslib; };
+  withDebug = import ./withDebug.nix;
+  wrapDebug = import ./wrapDebug.nix;
+  wrapSubstitutingDebug = import ./wrapSubstitutingDebug.nix;
+}
