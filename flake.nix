b798519b89d0c372f7255e4463526d8b9d1c16a9
nrs-status <sebaarsim@gmail.com>
2025-05-01 17:23:24 +0000

    adding defaultCabalConfig

--- flake.nix
+++ flake.nix
@@ -6,54 +6,61 @@
     prelibBootstrapNixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
-  outputs = inputs: 
-      let total = rec {
-        lclInputslessTypes = lclInputs: lclInputs.baselib.mkTypesAttrs { 
-          typesdir = ./mauso_halicarnassus; 
-          importsToPass = { inherit lclInputs; }; 
-        };
+  outputs = inputs:
+    let
+      total = rec {
+        lclInputslessTypes = lclInputs:
+          lclInputs.baselib.mkTypesAttrs {
+            typesdir = ./mauso_halicarnassus;
+            importsToPass = { inherit lclInputs; };
+          };
         nixpkgslessLclInputs = pkgs: rec {
-            pkgslib = pkgs.lib;
-            prelib = import ./bill_projects_belt { inherit pkgslib; };
-            tclib = import ./colossus_rhodes { inherit pkgslib prelib; };
-            baselib = import ./lighthouse_alexandria { inherit pkgslib prelib tclib; nixvimFlake = inputs.nixvimFlake; };
-            tc = tclib.tc;
+          pkgslib = pkgs.lib;
+          prelib = import ./bill_projects_belt { inherit pkgslib; };
+          tclib = import ./colossus_rhodes { inherit pkgslib prelib; };
+          baselib = import ./lighthouse_alexandria {
+            inherit pkgslib prelib tclib;
             nixvimFlake = inputs.nixvimFlake;
+          };
+          tc = tclib.tc;
+          nixvimFlake = inputs.nixvimFlake;
         };
-        supportedSystems = [
-          "x86_64-linux"
-        ];
+        supportedSystems = [ "x86_64-linux" ];
         nixpkgsConfig = {
           allowUnfree = true;
           android_sdk.accept_license = true;
         };
-        envsToProvide = [
-          "workEnv"
-        ];
+        envsToProvide = [ "workEnv" ];
         packagesToProvide = [
           [ "montezuma_circles_scroll" "nixvim" "base" ]
+          "defaultCabalConfig"
+
         ];
         wUnstable = {
           nixpkgs = inputs.nixpkgsUnstable;
-          inherit nixpkgsConfig supportedSystems envsToProvide packagesToProvide;
+          inherit nixpkgsConfig supportedSystems envsToProvide
+            packagesToProvide;
           lclInputs = nixpkgslessLclInputs;
           types = lclInputslessTypes;
         };
         w2411 = {
           nixpkgs = inputs.nixpkgs2411;
-          inherit nixpkgsConfig supportedSystems envsToProvide packagesToProvide;
+          inherit nixpkgsConfig supportedSystems envsToProvide
+            packagesToProvide;
           lclInputs = nixpkgslessLclInputs;
           types = lclInputslessTypes;
         };
-        bootstrappedPrelib = import ./bill_projects_belt { pkgslib = (import inputs.prelibBootstrapNixpkgs { system = "x86_64-linux"; }).lib; };
+        bootstrappedPrelib = import ./bill_projects_belt {
+          pkgslib = (import inputs.prelibBootstrapNixpkgs {
+            system = "x86_64-linux";
+          }).lib;
+        };
         mkOutputResult = (import ./lighthouse_alexandria/mkOutput.nix) {
           prelib = bootstrappedPrelib;
           envsdir = ./pyramid_giza;
           lclpkgsdir = ./temple_artemis_ephesus;
-          outputDeclAttrs = {
-            inherit wUnstable w2411;
-          };
+          outputDeclAttrs = { inherit wUnstable w2411; };
         };
       };
-      in total.mkOutputResult; 
+    in total.mkOutputResult;
 }
