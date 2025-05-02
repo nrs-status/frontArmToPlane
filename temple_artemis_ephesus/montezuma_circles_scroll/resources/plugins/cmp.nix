f6621d68e4519c778058d7989e210d619cd5d8cd
nrs-status <sebaarsim@gmail.com>
2025-05-02 02:05:04 +0000

    enabling cmp-path

--- temple_artemis_ephesus/montezuma_circles_scroll/resources/plugins/cmp.nix
+++ temple_artemis_ephesus/montezuma_circles_scroll/resources/plugins/cmp.nix
@@ -1,55 +1,48 @@
-{ lclInputs, types, system, activateDebug ? false, ...}:
+{ lclInputs, types, system, activateDebug ? false, ... }:
 with lclInputs;
 with types;
-let total = rec {
-  final = tc PluginSlice tcInput;
-  tcInput = {
-    cmp = {
-    enable = true;
-    autoEnableSources = true;
-    settings = {
-      mapping = {
-        __raw = ''
-          cmp.mapping.preset.insert({
-          ['<C-j>'] = cmp.mapping.select_next_item(),
-          ['<C-k>'] = cmp.mapping.select_prev_item(),
-          ['<C-c>'] = cmp.mapping.abort(),
+let
+  total = rec {
+    final = tc PluginSlice tcInput;
+    tcInput = {
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+        settings = {
+          mapping = {
+            __raw = ''
+              cmp.mapping.preset.insert({
+              ['<C-j>'] = cmp.mapping.select_next_item(),
+              ['<C-k>'] = cmp.mapping.select_prev_item(),
+              ['<C-c>'] = cmp.mapping.abort(),
 
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
 
-           ['<C-w>'] = cmp.mapping.scroll_docs(4),
-  
-           ['<C-Space>'] = cmp.mapping.complete(), --invokes completion
+               ['<C-w>'] = cmp.mapping.scroll_docs(4),
+
+               ['<C-Space>'] = cmp.mapping.complete(), --invokes completion
 
 
-           ['<C-CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-          })
-        '';
+               ['<C-CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+              })
+            '';
+          };
+          sources = [
+            { name = "cmdline"; }
+            { name = "kitty"; }
+            { name = "path"; }
+          ];
+        };
       };
-      sources = [
-        #{ --will cause errors if enabled, use the onehere below after the cmp block instead
-        #  name = "cmdline";
-        #}
-         {
-           name = "kitty";
-             }
-        # {
-        #   name = "sql";
-        # }
-      ];
+
+      cmp-cmdline.enable = true;
+      cmp-nvim-lsp.enable = true;
+      cmp-nvim-lua.enable = true;
+      cmp-rg.enable = true;
+      cmp-treesitter.enable = true;
+      cmp-zsh.enable = true;
+      cmp-path.enable = true;
+      cmp_luasnip.enable = true;
     };
   };
-
-  cmp-cmdline.enable = true;
-  cmp-nvim-lsp.enable = true;
-  cmp-nvim-lua.enable = true;
-  cmp-rg.enable = true;
-  cmp-treesitter.enable = true;
-  cmp-zsh.enable = true;
-  cmp-path.enable = true;
-  cmp_luasnip.enable = true;
-};
-};
-in prelib.wrapDebug {
-  inherit total activateDebug;
-}
+in prelib.wrapDebug { inherit total activateDebug; }
