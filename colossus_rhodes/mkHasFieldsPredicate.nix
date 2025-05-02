6f36e5f1c73dd39f5862ed11816cb1c07ec4eea5
nrs-status <sebaarsim@gmail.com>
2025-05-02 02:55:26 +0000

    adding some primitive types

--- colossus_rhodes/mkHasFieldsPredicate.nix
+++ colossus_rhodes/mkHasFieldsPredicate.nix
@@ -1,20 +1,27 @@
-{ baselib, pkgslib, activateDebug ? false }:
-{ fields }:
+{ prelib, pkgslib }:
+{ fields, activateDebug ? false }:
 with builtins;
-let total = rec {
-  isInExpectedFields = key: _val: elem key fields;
-  tagMissingFields = target: mapAttrs isInExpectedFields target;
-  failures = target: pkgslib.filterAttrs (_key: val: val == false) (tagMissingFields target);
-  predicate = rec {
-    predname = "hasFields";
-    function = target: { testresult = length (attrNames (failures target)) > 0; inherit failures; };
-    handler = { target, type }: 
-      if (function target).testresult then
-        throw ("${type.typeName} failed to typecheck due to hasFields predicate; the following fields are unexpected: ${attrNames result.failures}")
-      else target;
+let
+  total = rec {
+    isInExpectedFields = key: _val: elem key fields;
+    tagMissingFields = target: mapAttrs isInExpectedFields target;
+    failures = target:
+      pkgslib.filterAttrs (_key: val: val == false) (tagMissingFields target);
+    predicate = rec {
+      predname = "hasFields";
+      function = target: {
+        testresult = length (attrNames (failures target)) > 0;
+        inherit failures;
+      };
+      handler = { target, type }:
+        if (function target).testresult then
+          abort
+          ("${type.typeName} failed to typecheck due to hasFields predicate; the following fields are unexpected: ${
+              attrNames result.failures
+            }")
+        else
+          target;
+    };
+    final = predicate;
   };
-  final = predicate;
-  };
-}; in baselib.wrapDebug {
-  inherit total activateDebug;
-};
+in prelib.wrapDebug { inherit total activateDebug; }
