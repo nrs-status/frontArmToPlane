db1cc84018b72bd7f8027d8898da007b8f79d217
nrs-status <sebaarsim@gmail.com>
2025-06-08 14:18:49 +0000

    adding alaric_kicksdown_messi

--- temple_artemis_ephesus/alaric_kicksdown_messi/default.nix
+++ temple_artemis_ephesus/alaric_kicksdown_messi/default.nix
@@ -3,11 +3,19 @@
   final = pkgs.stdenv.mkDerivation {
     name = "alaric_kicksdown_messi";
     src = ./src;
+    nativeBuildInputs = [ pkgs.makeWrapper ];
     buildInputs = with pkgs; [ fzf ];
-    phases = ["installPhase"];
+    phases = ["installPhase" "fixupPhase"];
     installPhase = ''
       mkdir -p $out/bin
       cp -r $src/* $out/bin
+    '';
+    fixupPhase = ''
+      ls $out/bin
+      echo hi
+      for file in $out/bin; do
+        echo $file
+      done
     '';
   };
 }; in lclInputs.prelib.wrapDebug {
