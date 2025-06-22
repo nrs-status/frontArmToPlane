040796b42c2ae74ff084ed9db24711ee834fa28a
nrs-status <sebaarsim@gmail.com>
2025-06-22 02:56:13 +0000

    adding compile command

---
+++ temple_artemis_ephesus/montezuma_circles_scroll/resources/extraConfigLua/compile.lua
@@ -0,0 +1,8 @@
+local vim = vim
+local jar_path = "/home/sieyes/baghdad_plane/flakes/clj-env/clj-env/target/uberjar/clj-env-0.1.0-SNAPSHOT-standalone.jar"
+local function compile()
+  local absolute_path = vim.fn.expand("%:p")
+  local ran_command = vim.system({"java", "-jar", jar_path, absolute_path})
+  return ran_command:wait()
+end
+return vim.keymap.set("n", "<leader>e$c", compile)
