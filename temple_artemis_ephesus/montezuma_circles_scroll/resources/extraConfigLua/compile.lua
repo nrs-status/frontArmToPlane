37b7ee61642a5f9fd887a9033128ad63f397eaf1
nrs-status <sebaarsim@gmail.com>
2025-06-23 06:01:46 +0000

    adding debug printing to compile command

--- temple_artemis_ephesus/montezuma_circles_scroll/resources/extraConfigLua/compile.lua
+++ temple_artemis_ephesus/montezuma_circles_scroll/resources/extraConfigLua/compile.lua
@@ -1,7 +1,12 @@
 local jar_path = "'/home/sieyes/baghdad_plane/flakes/clj-env/clj-env/target/uberjar/clj-env-0.1.0-SNAPSHOT-standalone.jar'"
 local function compile()
   local absolute_path = table.concat({"'", vim.fn.expand("%:p"), "'"})
-  local ran_command = vim.fn.system(table.concat({"java", "-jar", jar_path, absolute_path}, " "))
+  local command = table.concat({"java", "-jar", jar_path, absolute_path}, " ")
+  local ran_command = vim.fn.system(command)
+  print("jar-path: ", jar_path)
+  print("absolute-path: ", absolute_path)
+  print("command: ", command)
+  print("ran_command: ", ran_command)
   return ran_command
 end
 vim.keymap.set("n", "<leader>e$c", compile)
