b71ed6c7c9d146c9f262f147d3f5a2d1fa7d35e1
nrs-status <sebaarsim@gmail.com>
2025-07-04 11:53:02 +0000

    paredit config

--- temple_artemis_ephesus/montezuma_circles_scroll/resources/extraConfigLua/pareditConfig.lua
+++ temple_artemis_ephesus/montezuma_circles_scroll/resources/extraConfigLua/pareditConfig.lua
@@ -2,6 +2,10 @@
 local paredit = require("nvim-paredit")
 paredit.setup({
   keys = {
+        ["<localleader>@u"] = { paredit.unwrap.unwrap_form_under_cursor, "Splice sexp" },
+
+        ["<localleader>@("] = { paredit.api.wrap_enclosing_form_under_cursor("(", ")"), "Splice sexp" },
+
 	  ["("] = {
       paredit.api.move_to_parent_form_start,
       "Jump to parent form's head",
