29bfb3489847b7778add8a24bcfdee27f7813753
nrs <32944119+nrs-status@users.noreply.github.com>
2026-02-02 15:40:26 +0000

    Update forLean.nix

--- temple_artemis_ephesus/montezuma_circles_scroll/envAttrs/forLean.nix
+++ temple_artemis_ephesus/montezuma_circles_scroll/envAttrs/forLean.nix
@@ -1,9 +1,34 @@
-{ lclInputs, ... }:
-lclInputs.baselib.extendNixvimEnvAttrs1 {
-  envAttrsToExtend = import ./base.nix {};
-  symlinkJoinName = "leanNixvimEnv";
-  plugins = [
-    ../resources/plugins/lean.nix
+{ pkgs, ... }:
+{
+  symlinkJoinName = "baseNixvimEnv";
+  etc = ../resources/etc.nix;
+  keymaps = ../resources/keymaps.nix;
+  opts = ../resources/opts.nix;
+  pluginsList = builtins.map (path : ../resources/plugins + path) [
+    /cmp.nix
+    /lsp.nix
+    /pluginsCore.nix
+    /lisps.nix
+    /webDevPlugins.nix
+    /lean.nix
+  ];
+  filetype = {};
+  extraPluginsPathList = builtins.map (path : ../resources/extraPlugins + path) [
+    /lispRelated.nix
+  ];
+  extraConfigLuaList = builtins.map (path: ../resources/extraConfigLua + path) [
+    /diagnosticsDefault.lua
+    /rememberFolds.lua
+    /tabKeyFunc.lua
+    /toggleAbsRelNums.lua
+    /compile.lua
+    /pareditConfig.lua
+    /def_and_eval.lua
+    /partial_comp_eval.lua
+    /conjure_introspection_atm.lua
+  ];
+  extraPackages = with pkgs; [
+    #used by conform-nvim
+    nixfmt # nix official formatter
   ];
 }
-
